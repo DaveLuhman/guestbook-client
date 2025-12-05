61ec9ea96f259a05b8034f0a6d7fade85d4b476e
Dave Luhman <Dave@ado.software>
2025-12-04 22:41:57 -0600

    Add dead code annotations to FileLogger and init_logging functions
    - Introduced #[allow(dead_code)] annotations to the FileLogger struct and init_logging function to suppress warnings for unused code, aiding in future development and refactoring efforts.

--- src-tauri/src/logging.rs
+++ src-tauri/src/logging.rs
@@ -5,6 +5,7 @@ use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 
+#[allow(dead_code)]
 pub struct FileLogger {
     log_dir: PathBuf,
     current_file: Arc<Mutex<Option<File>>>,
@@ -91,6 +92,7 @@ impl Log for FileLogger {
     }
 }
 
+#[allow(dead_code)]
 pub fn init_logging(config_dir: &Path) -> io::Result<()> {
     let file_logger = FileLogger::new(config_dir)?;
     log::set_boxed_logger(Box::new(file_logger))
